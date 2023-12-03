6853f4f5aab5e663cb56eed38b30a933c746ee5d
kwatch <kwatch@gmail.com>
2023-12-03 11:48:24 +0900

    move 'Builder' class to end of 'JetRouter' class

--- lib/rack/jet_router.rb
+++ lib/rack/jet_router.rb
@@ -87,226 +87,6 @@ module Rack
     REQUEST_METHODS = %w[GET POST PUT DELETE PATCH HEAD OPTIONS TRACE LINK UNLINK] \
                         .each_with_object({}) {|s, d| d[s] = s.intern }
 
-
-    class Builder
-
-      def initialize(router, enable_range=true)
-        @router = router
-        @enable_range = enable_range
-      end
-
-      def build_tree(mapping, &callback)
-        block_given_p = block_given?()
-        #; [!6oa05] builds nested hash object from mapping data.
-        tree = {}         # tree is a nested dict
-        param_d = {}
-        _traverse_mapping(mapping, "", mapping.class) do |path, item|
-          #; [!j0pes] if item is a hash object, converts keys from symbol to string.
-          item = _normalize_mapping_keys(item) if item.is_a?(Hash)
-          #; [!vfytw] handles urlpath pattern as variable when urlpath param exists.
-          has_param = (path =~ /:\w|\(.*?\)/)
-          if has_param
-            d = tree
-            sb = ['\A']
-            pos = 0
-            params = []
-            #; [!uyupj] handles urlpath parameter such as ':id'.
-            #; [!j9cdy] handles optional urlpath parameter such as '(.:format)'.
-            path.scan(/:(\w+)|\((.*?)\)/) do
-              param = $1; optional = $2         # ex: $1=='id' or $2=='.:format'
-              m = Regexp.last_match()
-              str = path[pos, m.begin(0) - pos]
-              pos = m.end(0)
-              #; [!akkkx] converts urlpath param into regexp.
-              pat1, pat2 = _param_patterns(param, optional) do |param_|
-                param_.freeze
-                params << (param_d[param_] ||= param_)
-              end
-              #; [!po6o6] param regexp should be stored into nested dict as a Symbol.
-              d = _next_dict(d, str) unless str.empty?
-              d = (d[pat1.intern] ||= {})       # ex: pat1=='[^./?]+'
-              #; [!zoym3] urlpath string should be escaped.
-              sb << Regexp.escape(str) << pat2  # ex: pat2=='([^./?]+)'
-            end
-            #; [!o642c] remained string after param should be handled correctly.
-            str = pos == 0 ? path : path[pos..-1]
-            unless str.empty?
-              d = _next_dict(d, str)
-              sb << Regexp.escape(str)          # ex: str=='.html'
-            end
-            sb << '\z'
-            #; [!kz8m7] range object should be included into tuple if only one param exist.
-            range = @enable_range ? _range_of_urlpath_param(path) : nil
-            #; [!c6xmp] tuple should be stored into nested dict with key 'nil'.
-            d[nil] = [Regexp.compile(sb.join()), params, item, range]
-          end
-          #; [!gls5k] yields callback if given.
-          yield path, item, !! has_param if block_given_p
-        end
-        return tree
-      end
-
-      private
-
-      def _traverse_mapping(mapping, base_path, mapping_class, &block)
-        #; [!9s3f0] supports both nested list mapping and nested dict mapping.
-        mapping.each do |sub_path, item|
-          full_path = base_path + sub_path
-          #; [!2ntnk] nested dict mapping can have subclass of Hash as handlers.
-          if item.class == mapping_class
-            #; [!dj0sh] traverses mapping recursively.
-            _traverse_mapping(item, full_path, mapping_class, &block)
-          else
-            #; [!brhcs] yields block for each full path and handler.
-            yield full_path, item
-          end
-        end
-      end
-
-      def _next_dict(d, str)
-        #; [!s1rzs] if new key exists in dict...
-        if d.key?(str)
-          #; [!io47b] just returns corresponding value and not change dict.
-          return d[str]
-        end
-        #; [!3ndpz] returns next dict.
-        d2 = nil          # next dict
-        c = str[0]
-        found = false
-        d.keys.each do |key|
-          if found
-            #; [!5fh08] keeps order of keys in dict.
-            d[key] = d.delete(key)
-          #; [!4wdi7] ignores Symbol key (which represents regexp).
-          #; [!66sdb] ignores nil key (which represents leaf node).
-          elsif key.is_a?(String) && key[0] == c
-            found = true
-            prefix, rest1, rest2 = _common_prefix(key, str)
-            #; [!46o9b] if existing key is same as common prefix...
-            if rest1.empty?
-              #; [!4ypls] not replace existing key.
-              val = d[key]
-              d2 = _next_dict(val, rest2)
-              break
-            #; [!veq0q] if new key is same as common prefix...
-            elsif rest2.empty?
-              #; [!0tboh] replaces existing key with ney key.
-              val = d.delete(key)
-              d2 = {rest1 => val}
-              d[prefix] = d2
-            #; [!esszs] if common prefix is a part of exsting key and new key...
-            else
-              #; [!pesq0] replaces existing key with common prefix.
-              val = d.delete(key)
-              d2 = {}
-              d[prefix] = {rest1 => val, rest2 => d2}
-            end
-          end
-        end
-        #; [!viovl] if new key has no common prefix with existing keys...
-        unless found
-          #; [!i6smv] adds empty dict with new key.
-          d2 = {}
-          d[str] = d2
-        end
-        return d2
-      end
-
-      def _common_prefix(str1, str2)    # ex: "/api/books/" and "/api/blog/"
-        #; [!86tsd] calculates common prefix of two strings.
-        #n = [str1.length, str2.length].min()
-        n1 = str1.length; n2 = str2.length
-        n = n1 < n2 ? n1 : n2
-        i = 0
-        while i < n && str1[i] == str2[i]
-          i += 1
-        end
-        #; [!1z2ii] returns common prefix and rest of strings.
-        prefix = str1[0...i]            # ex: "/api/b"
-        rest1  = str1[i..-1]            # ex: "ooks/"
-        rest2  = str2[i..-1]            # ex: "log/"
-        return prefix, rest1, rest2
-      end
-
-      def _param_patterns(param, optional, &callback)
-        #; [!j90mw] returns '[^./?]+' and '([^./?]+)' if param specified.
-        if param
-          optional == nil  or raise "** internal error"
-          yield param
-          pat1 = _param_pattern(param)
-          pat2 = "(#{pat1})"
-        #; [!raic7] returns '(?:\.[^./?]+)?' and '(?:\.([^./?]+))?' if optional param is '(.:format)'.
-        elsif optional == ".:format"
-          yield "format"
-          pat1 = '(?:\.[^./?]+)?'
-          pat2 = '(?:\.([^./?]+))?'
-        #; [!69yj9] optional string can contains other params.
-        elsif optional
-          sb = ['(?:']
-          optional.scan(/(.*?)(?::(\w+))/) do |str, param_|
-            pat = _param_pattern(param)               # ex: pat == '[^./?]+'
-            sb << Regexp.escape(str) << "<<#{pat}>>"  # ex: sb << '(?:\.<<[^./?]+>>)?'
-            yield param_
-          end
-          sb << Regexp.escape($' || optional)
-          sb << ')?'
-          s = sb.join()
-          pat1 = s.gsub('<<', '' ).gsub('>>', '' )    # ex: '(?:\.[^./?]+)?'
-          pat2 = s.gsub('<<', '(').gsub('>>', ')')    # ex: '(?:\.([^./?]+))?'
-        else
-          raise "** internal error"
-        end
-        return pat1, pat2
-      end
-
-      def _param_pattern(param)
-        return @router.param_pattern(param)    # ex: '[^./?]+'
-      end
-
-      def build_rexp(tree, &callback)
-        #; [!65yw6] converts nested dict into regexp.
-        sb = ['\A']
-        _build_rexp(tree, sb, &callback)
-        sb << '\z'
-        return Regexp.compile(sb.join())
-      end
-      public :build_rexp
-
-      def _build_rexp(nested_dict, sb, &b)
-        #; [!hs7vl] '(?:)' and '|' are added only if necessary.
-        sb << '(?:' if nested_dict.length > 1
-        nested_dict.each_with_index do |(k, v), i|
-          sb << '|' if i > 0
-          #; [!7v7yo] nil key means leaf node and yields block argument.
-          #; [!hda6m] string key should be escaped.
-          #; [!b9hxc] symbol key means regexp string.
-          case k
-          when nil    ; sb << '(\z)'           ; yield v
-          when String ; sb << Regexp.escape(k) ; _build_rexp(v, sb, &b)
-          when Symbol ; sb << k.to_s           ; _build_rexp(v, sb, &b)
-          else        ; raise "** internal error"
-          end
-        end
-        sb << ')' if nested_dict.length > 1
-      end
-
-      def _range_of_urlpath_param(urlpath_pattern)      # ex: '/books/:id/edit'
-        #; [!syrdh] returns Range object when urlpath_pattern contains just one param.
-        #; [!skh4z] returns nil when urlpath_pattern contains more than two params.
-        #; [!acj5b] returns nil when urlpath_pattern contains no params.
-        rexp = /:\w+|\(.*?\)/
-        arr = urlpath_pattern.split(rexp, -1)          # ex: ['/books/', '/edit']
-        return nil unless arr.length == 2
-        return (arr[0].length .. -(arr[1].length+1))   # ex: 7..-6  (Range object)
-      end
-
-      def _normalize_mapping_keys(dict)
-        return @router.normalize_mapping_keys(dict)
-      end
-
-    end
-
-
     def initialize(mapping, cache_size: 0, enable_range: true)
       @cache_size = cache_size
       @cache_dict = cache_size > 0 ? {} : nil
@@ -530,6 +310,226 @@ module Rack
       #return '\d+' if param == "id" || param =~ /_id\z/
       return '[^./?]+'
     end
+
+
+    class Builder
+
+      def initialize(router, enable_range=true)
+        @router = router
+        @enable_range = enable_range
+      end
+
+      def build_tree(mapping, &callback)
+        block_given_p = block_given?()
+        #; [!6oa05] builds nested hash object from mapping data.
+        tree = {}         # tree is a nested dict
+        param_d = {}
+        _traverse_mapping(mapping, "", mapping.class) do |path, item|
+          #; [!j0pes] if item is a hash object, converts keys from symbol to string.
+          item = _normalize_mapping_keys(item) if item.is_a?(Hash)
+          #; [!vfytw] handles urlpath pattern as variable when urlpath param exists.
+          has_param = (path =~ /:\w|\(.*?\)/)
+          if has_param
+            d = tree
+            sb = ['\A']
+            pos = 0
+            params = []
+            #; [!uyupj] handles urlpath parameter such as ':id'.
+            #; [!j9cdy] handles optional urlpath parameter such as '(.:format)'.
+            path.scan(/:(\w+)|\((.*?)\)/) do
+              param = $1; optional = $2         # ex: $1=='id' or $2=='.:format'
+              m = Regexp.last_match()
+              str = path[pos, m.begin(0) - pos]
+              pos = m.end(0)
+              #; [!akkkx] converts urlpath param into regexp.
+              pat1, pat2 = _param_patterns(param, optional) do |param_|
+                param_.freeze
+                params << (param_d[param_] ||= param_)
+              end
+              #; [!po6o6] param regexp should be stored into nested dict as a Symbol.
+              d = _next_dict(d, str) unless str.empty?
+              d = (d[pat1.intern] ||= {})       # ex: pat1=='[^./?]+'
+              #; [!zoym3] urlpath string should be escaped.
+              sb << Regexp.escape(str) << pat2  # ex: pat2=='([^./?]+)'
+            end
+            #; [!o642c] remained string after param should be handled correctly.
+            str = pos == 0 ? path : path[pos..-1]
+            unless str.empty?
+              d = _next_dict(d, str)
+              sb << Regexp.escape(str)          # ex: str=='.html'
+            end
+            sb << '\z'
+            #; [!kz8m7] range object should be included into tuple if only one param exist.
+            range = @enable_range ? _range_of_urlpath_param(path) : nil
+            #; [!c6xmp] tuple should be stored into nested dict with key 'nil'.
+            d[nil] = [Regexp.compile(sb.join()), params, item, range]
+          end
+          #; [!gls5k] yields callback if given.
+          yield path, item, !! has_param if block_given_p
+        end
+        return tree
+      end
+
+      private
+
+      def _traverse_mapping(mapping, base_path, mapping_class, &block)
+        #; [!9s3f0] supports both nested list mapping and nested dict mapping.
+        mapping.each do |sub_path, item|
+          full_path = base_path + sub_path
+          #; [!2ntnk] nested dict mapping can have subclass of Hash as handlers.
+          if item.class == mapping_class
+            #; [!dj0sh] traverses mapping recursively.
+            _traverse_mapping(item, full_path, mapping_class, &block)
+          else
+            #; [!brhcs] yields block for each full path and handler.
+            yield full_path, item
+          end
+        end
+      end
+
+      def _next_dict(d, str)
+        #; [!s1rzs] if new key exists in dict...
+        if d.key?(str)
+          #; [!io47b] just returns corresponding value and not change dict.
+          return d[str]
+        end
+        #; [!3ndpz] returns next dict.
+        d2 = nil          # next dict
+        c = str[0]
+        found = false
+        d.keys.each do |key|
+          if found
+            #; [!5fh08] keeps order of keys in dict.
+            d[key] = d.delete(key)
+          #; [!4wdi7] ignores Symbol key (which represents regexp).
+          #; [!66sdb] ignores nil key (which represents leaf node).
+          elsif key.is_a?(String) && key[0] == c
+            found = true
+            prefix, rest1, rest2 = _common_prefix(key, str)
+            #; [!46o9b] if existing key is same as common prefix...
+            if rest1.empty?
+              #; [!4ypls] not replace existing key.
+              val = d[key]
+              d2 = _next_dict(val, rest2)
+              break
+            #; [!veq0q] if new key is same as common prefix...
+            elsif rest2.empty?
+              #; [!0tboh] replaces existing key with ney key.
+              val = d.delete(key)
+              d2 = {rest1 => val}
+              d[prefix] = d2
+            #; [!esszs] if common prefix is a part of exsting key and new key...
+            else
+              #; [!pesq0] replaces existing key with common prefix.
+              val = d.delete(key)
+              d2 = {}
+              d[prefix] = {rest1 => val, rest2 => d2}
+            end
+          end
+        end
+        #; [!viovl] if new key has no common prefix with existing keys...
+        unless found
+          #; [!i6smv] adds empty dict with new key.
+          d2 = {}
+          d[str] = d2
+        end
+        return d2
+      end
+
+      def _common_prefix(str1, str2)    # ex: "/api/books/" and "/api/blog/"
+        #; [!86tsd] calculates common prefix of two strings.
+        #n = [str1.length, str2.length].min()
+        n1 = str1.length; n2 = str2.length
+        n = n1 < n2 ? n1 : n2
+        i = 0
+        while i < n && str1[i] == str2[i]
+          i += 1
+        end
+        #; [!1z2ii] returns common prefix and rest of strings.
+        prefix = str1[0...i]            # ex: "/api/b"
+        rest1  = str1[i..-1]            # ex: "ooks/"
+        rest2  = str2[i..-1]            # ex: "log/"
+        return prefix, rest1, rest2
+      end
+
+      def _param_patterns(param, optional, &callback)
+        #; [!j90mw] returns '[^./?]+' and '([^./?]+)' if param specified.
+        if param
+          optional == nil  or raise "** internal error"
+          yield param
+          pat1 = _param_pattern(param)
+          pat2 = "(#{pat1})"
+        #; [!raic7] returns '(?:\.[^./?]+)?' and '(?:\.([^./?]+))?' if optional param is '(.:format)'.
+        elsif optional == ".:format"
+          yield "format"
+          pat1 = '(?:\.[^./?]+)?'
+          pat2 = '(?:\.([^./?]+))?'
+        #; [!69yj9] optional string can contains other params.
+        elsif optional
+          sb = ['(?:']
+          optional.scan(/(.*?)(?::(\w+))/) do |str, param_|
+            pat = _param_pattern(param)               # ex: pat == '[^./?]+'
+            sb << Regexp.escape(str) << "<<#{pat}>>"  # ex: sb << '(?:\.<<[^./?]+>>)?'
+            yield param_
+          end
+          sb << Regexp.escape($' || optional)
+          sb << ')?'
+          s = sb.join()
+          pat1 = s.gsub('<<', '' ).gsub('>>', '' )    # ex: '(?:\.[^./?]+)?'
+          pat2 = s.gsub('<<', '(').gsub('>>', ')')    # ex: '(?:\.([^./?]+))?'
+        else
+          raise "** internal error"
+        end
+        return pat1, pat2
+      end
+
+      def _param_pattern(param)
+        return @router.param_pattern(param)    # ex: '[^./?]+'
+      end
+
+      def build_rexp(tree, &callback)
+        #; [!65yw6] converts nested dict into regexp.
+        sb = ['\A']
+        _build_rexp(tree, sb, &callback)
+        sb << '\z'
+        return Regexp.compile(sb.join())
+      end
+      public :build_rexp
+
+      def _build_rexp(nested_dict, sb, &b)
+        #; [!hs7vl] '(?:)' and '|' are added only if necessary.
+        sb << '(?:' if nested_dict.length > 1
+        nested_dict.each_with_index do |(k, v), i|
+          sb << '|' if i > 0
+          #; [!7v7yo] nil key means leaf node and yields block argument.
+          #; [!hda6m] string key should be escaped.
+          #; [!b9hxc] symbol key means regexp string.
+          case k
+          when nil    ; sb << '(\z)'           ; yield v
+          when String ; sb << Regexp.escape(k) ; _build_rexp(v, sb, &b)
+          when Symbol ; sb << k.to_s           ; _build_rexp(v, sb, &b)
+          else        ; raise "** internal error"
+          end
+        end
+        sb << ')' if nested_dict.length > 1
+      end
+
+      def _range_of_urlpath_param(urlpath_pattern)      # ex: '/books/:id/edit'
+        #; [!syrdh] returns Range object when urlpath_pattern contains just one param.
+        #; [!skh4z] returns nil when urlpath_pattern contains more than two params.
+        #; [!acj5b] returns nil when urlpath_pattern contains no params.
+        rexp = /:\w+|\(.*?\)/
+        arr = urlpath_pattern.split(rexp, -1)          # ex: ['/books/', '/edit']
+        return nil unless arr.length == 2
+        return (arr[0].length .. -(arr[1].length+1))   # ex: 7..-6  (Range object)
+      end
+
+      def _normalize_mapping_keys(dict)
+        return @router.normalize_mapping_keys(dict)
+      end
+
+    end
+
 
   end
 
