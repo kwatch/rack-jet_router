1a59f88ed660eb92137eae175db0414949fba7fd
kwatch <kwatch@gmail.com>
2024-01-04 15:03:25 +0900

    fix 'bench' task not to raise error when arg is missing

--- bench/Rakefile.rb
+++ bench/Rakefile.rb
@@ -5,7 +5,7 @@
 desc "run 'bench.rb' script"
 task :bench, :N do |t, args|
   n = args[:N] || 1000000
-  ruby "bench.rb", "-n", n
+  ruby "bench.rb", "-n", n.to_s
   #ruby "bench.rb", "-n", n, "--sinatra=0", "--multiplexer=0"
   #ruby "bench.rb", "-n", n, "--rack=0", "--jetrouter=0", "--keight=0", "--multiplexer=0", "--sinatra=0", "--hanami=0"
 end
